79879263f3f4ee4ad1bdef6a9c5cb5f77e25ac98
Picorims <picorims.contact@gmail.com>
2023-06-14 14:04:30 +0200

    add prototype for sphere sphere collision

--- src/first_prog.cpp
+++ src/first_prog.cpp
@@ -352,14 +352,14 @@ int main(int argc, char* args[])
         }
         // Create here specific forms and add them to the list...
         // Don't forget to update the actual number_of_forms !
-        Plan *pFace = NULL;
+        /*Plan *pFace = NULL;
         pFace = new Plan(Vector(1,0,0), Vector(0,1,0), Point(-0.5, -0.5, -0.5), 1, 1, ORANGE); // For the cube
         pFace = new Plan(Vector(1,0,0), Vector(0,1,0), Point(0.5, 0, 0.5), 1, 1, WHITE); // For the animation
         pFace->setTexture(textureid_1);
         forms_list[number_of_forms] = pFace;
         number_of_forms++;
 
-        engine.addForm(*pFace);
+        engine.addForm(pFace);*/
 
 //        pFace = new Plan(Vector(1,0,0), Vector(0,1,0), Point(-0.5, -0.5, 0.5), 1, 1, RED);
 //        forms_list[number_of_forms] = pFace;
@@ -381,28 +381,28 @@ int main(int argc, char* args[])
         Sphere* pSphere = NULL;
         Animation sphAnim;
         pSphere = new Sphere(0.4, WHITE);
-        sphAnim.setPos(Point(1.5,0,0));
+        sphAnim.setPos(Point(0,0,0));
         sphAnim.setPhi(0.1); // angle en degre
         sphAnim.setTheta(0.2); // angle en degre
-        sphAnim.setSpeed(Vector(-0.1,0,0)); // v initiale colineaire a Ox
+        sphAnim.setSpeed(Vector(0,0.2,-0.1)); // v initiale colineaire a Ox
         pSphere->setAnim(sphAnim);
         pSphere->setTexture(textureid_1);
         pSphere->getAnim().setPhi(10);
         forms_list[number_of_forms] = pSphere;
         number_of_forms++;
 
-        engine.addForm(*pSphere);
+        engine.addForm(pSphere);
 
         pSphere = new Sphere(0.3, RED);
         Animation sphAnim2;
-        sphAnim2.setPos(Point(1,1,0));
-        sphAnim2.setSpeed(Vector(-0.2,-0.2,0)); // v initiale dans plan x0y
+        sphAnim2.setPos(Point(0,1,0));
+        sphAnim2.setSpeed(Vector(0,0,-0.2)); // v initiale dans plan x0y
         pSphere->setAnim(sphAnim2);
         pSphere->setTexture(textureid_2);
         forms_list[number_of_forms] = pSphere;
         number_of_forms++;
 
-        engine.addForm(*pSphere);
+        engine.addForm(pSphere);
 
         // Get first "current time"
         previous_time_anim = previous_time_render = SDL_GetTicks();
--- src/physics.cpp
+++ src/physics.cpp
@@ -1,22 +1,74 @@
 #include "physics.h"
+#include <cmath>
 
-void CollisionEngine::collision(Sphere &sphere, Plan &plan)
+void CollisionEngine::collision(Sphere* sphere, Plan* plan)
 {
     std::cout << "sphere plan" << std::endl;
 }
 
-void CollisionEngine::collision(Sphere &sphere1, Sphere &sphere2)
+void CollisionEngine::collision(Sphere* sphere1, Sphere* sphere2)
 {
     std::cout << "sphere sphere" << std::endl;
+    // sphere1 with center A, sphere2 with center B
+    // v1 = speed of sphere1
+    // v1AB = v1 aligned on AB
+    // v1Perp = v1 perpendicular to AB
+    Vector ab = Vector(sphere1->getAnim().getPos(), sphere2->getAnim().getPos());
+    double r1 = sphere1->getRadius();
+    double r2 = sphere2->getRadius();
+
+    std::cout << ab.norm() << " " << r1+r2 << std::endl;
+    if (ab.norm() <= (r1 + r2)) {
+        std::cout << "collision !!!!!!!!!!!!!!!!!!!!!" << std::endl;
+        Vector v1 = sphere1->getAnim().getSpeed();
+        Vector v2 = sphere2->getAnim().getSpeed();
+
+        // assume s1 moves and s2 do not move
+        v1 = v1 - v2;
+        v2 = 0;
+
+        // find v2'
+        double abNorm = ab.norm();
+        Vector abUnit = ab * (1.0 / ab.norm()); // TODO TROUVER POURQUOI 0
+        Vector v1AB = (v1 * abUnit) * abUnit; // v1_projected_on_AB = (length_on_AB) * AB_unit_vector
+
+        // find v1'
+
+        // search the unit vector perpendicular to AB which is in the plan (v1, AB).
+        // 1) get the norm
+        // 2) rotate abUnit around the norm by 90°
+        // https://fr.wikipedia.org/wiki/Matrice_de_rotation
+
+        Vector normV1AB = v1 ^ ab;
+
+        // rotation
+        const double PI = std::acos(-1);
+        double c = std::cos(PI / 2);
+        double s = std::sin(PI / 2);
+        Vector abUnitPerp = Vector();
+        Vector u = abUnit;
+        abUnitPerp.x = (u.x*u.x*(1-c)+c    )*u.x  +  (u.x*u.y*(1-c)-u.z*s)*u.y  +  (u.x*u.z*(1-c)+u.y*s)*u.z;
+        abUnitPerp.y = (u.x*u.y*(1-c)+u.z*s)*u.x  +  (u.y*u.y*(1-c)+c    )*u.y  +  (u.y*u.z*(1-c)-u.x*s)*u.z;
+        abUnitPerp.z = (u.x*u.z*(1-c)-u.y*s)*u.x  +  (u.y*u.z*(1-c)+u.x*s)*u.y  +  (u.z*u.z*(1-c)+c    )*u.z;
+
+        Vector v1Perp = (v1 * abUnitPerp) * abUnitPerp;
+
+        Vector v1Prim = v1Perp;
+        Vector v2Prim = v1AB;
+
+        // apply new vectors
+        sphere1->getAnim().setSpeed(Vector(v1Prim + v2));
+        sphere2->getAnim().setSpeed(Vector(v2Prim + v2));
+    }
 }
 
-void CollisionEngine::addForm(Sphere &form)
+void CollisionEngine::addForm(Sphere* form)
 {
     std::cout << "added sphere" << std::endl;
     sphere_list.push_back(form);
 }
 
-void CollisionEngine::addForm(Plan &form)
+void CollisionEngine::addForm(Plan* form)
 {
     std::cout << "added plan" << std::endl;
     plan_list.push_back(form);
